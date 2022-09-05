dd02c8c4666f600db6d9e150380b1b9642d7823e
rutraq <a3332133@gmail.com>
2022-09-05 11:57:32 +0200

    cases animation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -68,6 +68,27 @@ async function firstToSecond() {
     opacity: 1,
     transition: "0s"
   });
+  await studyCaseAnimate();
+}
+
+async function studyCaseAnimate() {
+  let cases = $(".study-case");
+  for (let i = 0; i < cases.length; i++) {
+    $(cases[i]).css({position: 'relative'});
+  }
+  $(cases[1]).css({transform: "scale(1.2)", transition: "1.5s"});
+  $(cases[1]).find("img").css({transform: "rotate(2deg)", transition: "1.5s"});
+  $(cases[1]).animate({left: "-=15%"}, 2000, 'linear', async function () {
+    $(this).animate({left: "-=35%"}, 1000, 'linear').fadeTo(1500, 0.6);
+    $(cases[1]).css({transform: "scale(.55)", transition: "3.5s"}).removeClass("active");
+    $(cases[2]).animate({left: "-=40%"}, 3000);
+    await sleep(3000);
+    $(cases[2]).css({transform: "scale(1.2)", transition: "1s"}).addClass("active");
+  });
+  await sleep(2000);
+  $(cases[1]).find("img").css({transform: "rotate(0deg)", transition: "2s"});
+
+
 }
 
 function secondToThird() {
